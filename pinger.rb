50608f02a9196621b67afbee5547328c146599b6
Manoj Dayaram <noj@moovweb.com>
2013-01-13 23:17:02 -0800

    Using ruby instead of shell, so much cleaner.

---
+++ pinger.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'httpclient'
+
+sleep_time = 55 * 60  # 55 minutes
+
+sites = [
+  "http://noj.cc",
+  "http://i.hazasite.com",
+  "http://organs.hazasite.com",
+  "http://steroids.hazasite.com",
+  ]
+
+loop do
+  sites.each do |s|
+    print "Pinging #{s} .........."
+    res = HTTPClient.get(s)
+    puts " #{res.status_code}"
+  end
+  puts "Done, sleeping for #{sleep_time}s...."
+  sleep sleep_time
+  puts "Waking up!"
+end
