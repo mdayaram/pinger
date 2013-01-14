0ec7e08d1b2f083ecba7958134721fef69989bba
Manoj Dayaram <noj@moovweb.com>
2013-01-13 23:32:41 -0800

    daemonizing and syslog"

--- pinger.rb
+++ pinger.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'httpclient'
+require 'syslog'
 
 sleep_time = 55 * 60  # 55 minutes
 
@@ -10,13 +11,31 @@ sites = [
   "http://steroids.hazasite.com",
   ]
 
-loop do
-  sites.each do |s|
-    print "Pinging #{s} .........."
-    res = HTTPClient.get(s)
-    puts " #{res.status_code}"
+def puts(message)
+  Syslog.open($0, Syslog::LOG_PID | Syslog::LOG_CONS) do |s| 
+    s.info message
   end
-  puts "Done, sleeping for #{sleep_time}s...."
-  sleep sleep_time
-  puts "Waking up!"
 end
+
+Process.fork() {
+
+  Process.setsid
+  raise 'Second fork failed' if (pid = Process.fork()) == -1
+  exit unless pid.nil?
+
+  File.umask 0000
+  STDIN.reopen '/dev/null'
+  STDOUT.reopen '/dev/null', 'a'
+  STDERR.reopen '/dev/null', 'a'
+
+  loop do
+    sites.each do |s|
+      puts "* Pinging #{s} .........."
+      res = HTTPClient.get(s)
+      puts "+++ #{res.status_code}"
+    end
+    puts "Done, sleeping for #{sleep_time}s...."
+    sleep sleep_time
+    puts "Waking up!"
+  end
+}
