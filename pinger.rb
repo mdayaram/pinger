ba99e6074b4d91f956cb25cb87959dc915a92940
Manoj Dayaram <noj@moovweb.com>
2013-01-13 23:39:23 -0800

    removing daemonizing

--- pinger.rb
+++ pinger.rb
@@ -11,23 +11,6 @@ sites = [
   "http://steroids.hazasite.com",
   ]
 
-def puts(message)
-  Syslog.open($0, Syslog::LOG_PID | Syslog::LOG_CONS) do |s| 
-    s.info message
-  end
-end
-
-Process.fork() {
-
-  Process.setsid
-  raise 'Second fork failed' if (pid = Process.fork()) == -1
-  exit unless pid.nil?
-
-  File.umask 0000
-  STDIN.reopen '/dev/null'
-  STDOUT.reopen '/dev/null', 'a'
-  STDERR.reopen '/dev/null', 'a'
-
   loop do
     sites.each do |s|
       puts "* Pinging #{s} .........."
@@ -38,4 +21,3 @@ Process.fork() {
     sleep sleep_time
     puts "Waking up!"
   end
-}
