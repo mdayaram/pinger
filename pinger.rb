294c391745ae38fe3e32225eae3d8b5cb2d7adaf
Manoj Dayaram <noj@moovweb.com>
2015-03-23 02:58:42 -0700

    using heroku api to ping sites now

--- pinger.rb
+++ pinger.rb
@@ -5,6 +5,15 @@ require 'json'
 
 sleep_time = 55 * 60  # 55 minutes
 
+def puts(msg)
+  $stderr.puts(msg)
+end
+
+def print(msg)
+  $stderr.print(msg)
+  $stderr.flush
+end
+
 def sites
   extheader = {
     "Accept" => "application/vnd.heroku+json; version=3",
@@ -12,20 +21,27 @@ def sites
   }
 
   clnt = HTTPClient.new
-  res = clnt.get_content("https://api.heroku.com/apps", {}, extheader)
-  if res.status.successful?
-    parsed = json.parse(res.body)
-    s = []
+  clnt.ssl_config.ssl_version = :TLSv1
+  res = clnt.get("https://api.heroku.com/apps", {}, extheader)
+  s = []
+  if HTTP::Status.successful? res.status
+    parsed = JSON.parse(res.body)
     parsed.each do |p|
       s << "#{p["name"]}.herokuapp.com"
     end
+  else
+    puts "ERROR STATUS: #{res.status}"
+    puts "ERROR BODY:\n"
+    puts res.body
   end
+  s
 end
 
 loop do
   sites.each do |s|
+    print "* Pinged #{s} .......... "
     res = HTTPClient.get("http://#{s}")
-    puts "* Pinged #{s} .......... #{res.status_code}"
+    puts "\r* Pinged #{s} .......... #{res.status_code}"
   end
   puts "Done, sleeping for #{sleep_time}s...."
   sleep sleep_time
